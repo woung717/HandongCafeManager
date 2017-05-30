fc29271232090833e476e1c12151ec219d68cadc
woung717 <woung717@gmail.com>
2017-05-31 00:51:50 +0900

    basic frames

--- src/Customer.java
+++ src/Customer.java
@@ -1,5 +1,16 @@
+import java.sql.Connection;
+
 /**
  * Created by Shin on 2017-05-31.
  */
 public class Customer {
+    private Connection conn;
+
+    public Customer(Connection conn) {
+        this.conn = conn;
+    }
+
+    public String[] getCafeList() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Main {
+    static final String SERVER_ADDR = "peace.handong.edu";
+    static final String DB_ID = "db21100380";
+    static final String DB_PASSWORD = "21100380";
+    static final String DB_NAME = "21100380_HyeonUng_Shin";
 
     public static void main(String[] args) {
-	// write your code here
+        Connection conn = initDatabase(SERVER_ADDR, DB_ID, DB_PASSWORD);
+
+        Customer customer = new Customer(conn);
+    }
+
+    private static Connection initDatabase(String addr, String id, String password) {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            con = DriverManager.getConnection("jdbc:mysql://" + addr, id, password);
+            if (!con.isClosed())
+                System.out.println("Successfully connected to MySQL server.");
+
+            Statement stmt = (Statement) con.createStatement();
+
+            stmt.execute("USE " + DB_NAME);
+        }
+        catch(Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+        }
+
+        return con;
     }
 }
